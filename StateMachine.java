137f1b3f70880e657cf49572da9674eed9c4f464
Ioan-Valentin Voiculescu <ioan_voiculescu@icloud.com>
2017-11-27 23:57:30 +0200

    Update StateMachine.java

--- StateMachine.java
+++ StateMachine.java
@@ -1,11 +1,20 @@
-import java.util.List;
-import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by ioan on 11/12/17.
  */
 public abstract class StateMachine {
     protected String sc;
-    protected Map<String, List<String>> table;
-    public abstract void transition(String actiune);
+    protected HashMap<String,HashMap<String, String>> table;
+    
+    public void transition(String actiune){
+        int sw=0;
+        for(int i=0;(i<table.get(sc).size()) && (sw==0);i++) {
+            if (table.get(sc).get(actiune) != null) {
+                sc = table.get(sc).get(actiune);
+                sw = 1;
+            }
+        }
+    }
+    
 }

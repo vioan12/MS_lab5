b611d6dd5bba8ae5ee453e41a8bc1b96e7655d31
Ioan-Valentin Voiculescu <ioan_voiculescu@icloud.com>
2017-11-28 00:08:45 +0200

    Update StateMachine.java

--- StateMachine.java
+++ StateMachine.java
@@ -6,15 +6,11 @@ import java.util.HashMap;
 public abstract class StateMachine {
     protected String sc;
     protected HashMap<String,HashMap<String, String>> table;
-    
+
     public void transition(String actiune){
-        int sw=0;
-        for(int i=0;(i<table.get(sc).size()) && (sw==0);i++) {
-            if (table.get(sc).get(actiune) != null) {
+        if (table.get(sc).get(actiune) != null) {
                 sc = table.get(sc).get(actiune);
-                sw = 1;
-            }
         }
     }
-    
+
 }
